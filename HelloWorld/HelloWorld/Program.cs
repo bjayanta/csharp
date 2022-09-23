e767373e80a062134c73993af7e8bd88f1351287
Jayanta <bjayanta.me@gmail.com>
2022-09-23 19:51:04 +0600

    hello world programm

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            // Console.ReadLine();
+
+            System.Console.WriteLine("Hello Jayanta!");
+            // Console.ReadLine();
+
+            System.Console.WriteLine("Line 1");
+            System.Console.WriteLine("Line 2");
+            System.Console.WriteLine("Line 3");
+            System.Console.WriteLine("Line 4");
+            System.Console.WriteLine("Line 5");
+            System.Console.WriteLine("Line 6");
+            System.Console.WriteLine("Line 7");
+            System.Console.WriteLine("Line 8");
+            System.Console.WriteLine("Line 9");
+            System.Console.WriteLine("Line 10");
+
+            Console.ReadLine();
+        }
+    }
+}
