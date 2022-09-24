97b019576442db6781c6d9e094ec1f83d03deb1e
Jayanta <bjayanta.me@gmail.com>
2022-09-24 21:34:13 +0600

    init branch

---
+++ DatatypesLiteralsAndVariables/DatatypesLiteralsAndVariables/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatatypesLiteralsAndVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 1;
+            int i = 100;
+            float f = 2.5f;
+            double h = 3.7;
+            decimal d = 3.14m;
+            char c = 'a';
+            bool l = true;
+
+            Console.WriteLine(b);
+            Console.WriteLine(i);
+            Console.WriteLine(f);
+            Console.WriteLine(h);
+            Console.WriteLine(d);
+            Console.WriteLine(c);
+            Console.WriteLine(l);
+
+            Console.ReadLine();
+        }
+    }
+}
